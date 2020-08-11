57b3613a43e3772b33772ff12bb35629f5fc013a
Hector SANCHEZ <hectorsanchez@hectors-mbp.lan>
2020-08-11 02:38:14 -0400

    Adds user update endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,12 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: %i[show update]
+
+  # GET /users/:id
   def show
-    render json: User.find(params[:id])
+    render json: @user
   end
 
+  # POST /user
   def create
     @user = User.new(user_params)
 
@@ -13,7 +17,20 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  # PUT/PATCH /users/:id
+  def update
+    if @user.update(user_params)
+      render json: @user, status: :ok
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
   private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
 
     def user_params
       params.require(:user).permit(:email, :password)
