6f3901d45ea703cd1c7cb8a4df2d1801db411504
Hector SANCHEZ <hectorsanchez@hectors-mbp.lan>
2020-08-12 02:31:53 -0400

    Restrict actions for unauthorized users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: %i[show update destroy]
+  before_action :check_owner, only: %i[destroy update]
 
   # GET /users/:id
   def show
@@ -41,5 +42,9 @@ class Api::V1::UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:email, :password)
+    end
+
+    def check_owner
+      head :forbidden unless @user.id == current_user&.id
     end
 end
