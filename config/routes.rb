62321c04870dfc83175405ac4a6d7b3c8e78c50b
Hector SANCHEZ <hectorsanchez@hectors-mbp.lan>
2020-08-12 00:08:44 -0400

    Sets up tokens controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json  } do
     namespace :v1 do
       resources :users, only: %i[create destroy show update]
+      resources :tokens, only: [:create]
     end
   end
 end
--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -2,7 +2,7 @@ class JsonWebToken
   SECRET_KEY = Rails.application.secrets.secret_key_base.to_s
 
   def self.encode(payload, exp = 4.hours.from_now)
-    payload[:exp] = exp
+    payload[:exp] = exp.to_i
     JWT.encode(payload, SECRET_KEY)
   end
 
