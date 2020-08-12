51a73f063bddfaffaaca70a43ade21fb96cd4f9c
Hector SANCHEZ <hectorsanchez@hectors-mbp.lan>
2020-08-12 16:49:34 -0400

    Generates product model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -16,4 +16,10 @@ class UserTest < ActiveSupport::TestCase
     user = User.new(email: other_user.email, password_digest: "testering")
     assert_not user.valid?
   end
+
+  test "destroy user should destroy associated product(s)" do
+    assert_difference("Product.count", -1) do
+      users(:one).destroy
+    end
+  end
 end
