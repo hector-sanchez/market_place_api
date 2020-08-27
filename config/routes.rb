f7d9233ce9485cebc09019795422d76bee4fcc73
Hector SANCHEZ <hectorsanchez@hectors-mbp.lan>
2020-08-27 01:52:23 -0400

    Adds teh index action endpoint for the order

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       resources :users, only: %i[create destroy show update]
       resources :tokens, only: [:create]
       resources :products
+      resources :orders, only: :index
     end
   end
 end
