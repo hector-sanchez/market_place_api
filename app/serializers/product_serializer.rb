4cfa8a1e870af4af14b897a6b6ecc351d3f61bbe
Hector SANCHEZ <hectorsanchez@hectors-mbp.lan>
2020-08-29 01:04:31 -0400

    Adds caching to the serializers

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,5 +1,8 @@
 class ProductSerializer
   include FastJsonapi::ObjectSerializer
+
   attributes :title, :price, :published
   belongs_to :user
+
+  cache_options enabled: true, cache_length: 12.hours
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,8 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
+
   attributes :email
   has_many :products
+
+  cache_options enabled: true, cache_length: 12.hours
 end
